63b82b26e75678ba4b56ce097b2aa8edcfa84290
yovelcohen <yovell04@gmail.com>
2020-05-01 21:21:06 +0300

    Base Team Class

--- App/Team.py
+++ App/Team.py
@@ -1,14 +1,19 @@
-from App.TestCases import TEST_NAMES, TEST_WEIGHTS, TEST_MAX_CARRIES
-
-
 class Person:
     def __init__(self, name, max_allowed, mefaked=None):
         self.name = name
-        self.max_allowed = max_allowed
+        self._max_allowed = max_allowed
         self.mefaked = mefaked
 
-    def get_max_allowed(self):
-        return self.max_allowed
+    assigned_items = {}
+    bag = None
+
+    @property
+    def max_allowed(self):
+        return self._max_allowed
+
+    @max_allowed.setter
+    def max_allowed(self, value):
+        self._max_allowed = value
 
     @staticmethod
     def get_max_carry_in_kg(max_allowed, weight):
@@ -23,23 +28,3 @@ class Person:
         """
         Check that with every added item to what this person carries he's not passing his max allowed.
         """
-
-
-class Team:
-    team = None
-    if team is None:
-        team = []
-
-    @classmethod
-    def sort_people_data(cls, names, weights, max_carries):
-        weight_data = list(zip(weights, max_carries))
-        max_allowed = [Person.get_max_carry_in_kg(max_allowed=max_allowed, weight=weight) for max_allowed, weight in
-                       weight_data]
-        for name, max_allowed in zip(names, max_allowed):
-            person = Person(name=name, max_allowed=max_allowed)
-            cls.team.append(person)
-            return cls.team
-
-
-if __name__ == '__main__':
-    Team.sort_people_data(names=TEST_NAMES, weights=TEST_WEIGHTS, max_carries=TEST_MAX_CARRIES)
