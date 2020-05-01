cb75d279f9177ff3c6fcdd922fd8b9968b6e971a
yovelcohen <yovell04@gmail.com>
2020-05-01 21:19:52 +0300

    New
    Consts

--- App/Consts.py
+++ App/Consts.py
@@ -1,10 +1,5 @@
 from pandas import read_csv
 
-PLANES_LEN = 6
-PODS_LEN = 4
-GROUND_SYSTEM_LEN = 13
-LOW_BAG = 7
-MEFAKED_LOW = 14
 # Base DataFrame containing all items.
 ITEMS_DF = read_csv("/home/yovel/PycharmProjects/BagsFormatter/Resources/Items.csv",
                     dtype={'Item': 'str',
@@ -15,23 +10,43 @@ ITEMS_DF = read_csv("/home/yovel/PycharmProjects/BagsFormatter/Resources/Items.c
                            },
                     )
 
-BAGS_LENS = {
-    PLANES_LEN: 'planes bag',
-    PODS_LEN: 'pods bag',
-    GROUND_SYSTEM_LEN: 'ground system bag',
-    LOW_BAG: 'low bag',
-    MEFAKED_LOW: 'mefaked low'
+
+class BagsStringLength:
+    """
+    Lens of all bags strings.
+    And dictionary that contains string representing those lens.
+    """
+
+    PLANES_LEN = 6
+    PODS_LEN = 4
+    GROUND_SYSTEM_LEN = 13
+    LOW_BAG = 7
+    MEFAKED_LOW = 14
+
+    lens_list = [PLANES_LEN, PODS_LEN, GROUND_SYSTEM_LEN, LOW_BAG, MEFAKED_LOW]
+
+
+BAGS = {
+    BagsStringLength.PLANES_LEN: 'planes bag',
+    BagsStringLength.PODS_LEN: 'pods bag',
+    BagsStringLength.GROUND_SYSTEM_LEN: 'ground system bag',
+    BagsStringLength.LOW_BAG: 'low bag',
+    BagsStringLength.MEFAKED_LOW: 'mefaked low'
 }
 
+# Base Index
 INDEX = 0
 
+# Home Directory
 PATH = '/home/yovel/PycharmProjects/BagsFormatter/'
 
+# resources folder
 RESOURCES = 'Resources/'
 UPDATED_ITEMS_FILE = 'Updated_items.csv'
 
 
-class PandasConsts:
+# The items df consts.
+class DfConsts:
     BAGS = 'Bags'
     BAGS_COLUMN_LENGTH = 'bags column length'
     ASSIGNED_BAG = 'assigned_bag'
@@ -39,9 +54,13 @@ class PandasConsts:
     MUST_QUANTITY = 'must quantity'
     QUANTITY = 'quantity'
     ITEM = 'Item'
+    V = 'v'
+    WEIGHT = 'Weight'
+    CATEGORY = 'category'
 
 
+# for defining of types in df.
 class TypesConsts:
-    STRING = 'string'
+    STR = 'string'
     FLOAT = 'float'
     INT = 'int'
